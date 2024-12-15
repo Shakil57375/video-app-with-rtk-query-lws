2634228f0bf47283ba6fd9b3660453e50f5ddce0
Shakil57375 <shakil57375@gmail.com>
2024-12-15 23:02:28 +0600

    Get single video added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,18 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-      baseUrl: "http://localhost:9000",  // Ensure this URL is correct and server is running
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000", // Ensure this URL is correct and server is running
+  }),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => "/videos", // This should fetch from http://localhost:9000/videos
     }),
-    endpoints: (builder) => ({
-      getVideos: builder.query({
-        query: () => "/videos",  // This should fetch from http://localhost:9000/videos
-      }),
+    getVideo: builder.query({
+      query: (videoId) => `/videos/${videoId}`,
     }),
-  });
-  
-
+  }),
+});
 
 // export
-export  const {useGetVideosQuery} = apiSlice
+export const { useGetVideosQuery, useGetVideoQuery } = apiSlice;
