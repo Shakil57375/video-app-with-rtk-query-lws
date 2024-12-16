3d9d8a2b501285a14d3b505ea52aa29eb8263632
Shakil57375 <shakil57375@gmail.com>
2024-12-16 18:07:16 +0600

    related videos shown for co-responding video

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000", // Ensure this URL is correct and server is running
+    baseUrl: "http://localhost:3000", // Ensure this URL is correct and server is running
   }),
   endpoints: (builder) => ({
     getVideos: builder.query({
@@ -12,8 +12,16 @@ export const apiSlice = createApi({
     getVideo: builder.query({
       query: (videoId) => `/videos/${videoId}`,
     }),
+    getRelatedVideo: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`)
+        const queryString = `/videos?${likes.join("&")}`
+        return queryString;
+      }
+    })
   }),
 });
 
 // export
-export const { useGetVideosQuery, useGetVideoQuery } = apiSlice;
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideoQuery } = apiSlice;
